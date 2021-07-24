dd3a0d087e2b68569d4a8f9693c59e8de82f26ce
avilalopes <82402280+avilalopes@users.noreply.github.com>
2021-07-23 22:08:29 -0300

    Add files via upload

---
+++ Distancia/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Desafio {
+    static void Main() {
+        Console.WriteLine("Informe a distância entre a moto Y e a moto X:");
+        int quilometros = Int32.Parse(Console.ReadLine());
+        int minutos = quilometros * 2; // Digite aqui o calculo dos minutos
+        Console.WriteLine(minutos + " minutos");
+    }
+}
